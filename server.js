f39f08e2cd1e6648d44079ccd3b258ccc84b61a1
pmonie999 <Pmonie995@gmail.com>
2025-01-23 12:48:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "youngblessed955@gmail.com";
 const pass = "depmxuywrgznkhuq";
-// 6 dec
+// 4 jan
 
 // Middleware
 app.use(express.json());
